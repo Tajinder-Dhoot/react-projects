117b1637f9b30e1a98b4e01529adba9d3359e6d3
Tajinder Singh Dhoot <singhtazinder047@gmail.com>
2021-04-05 15:01:13 -0400

    Basic ToDo App

---
+++ 2to-do-list/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  if (true) {
+    const [toDos, setToDos] = useState([]);
+  }
+  const [input, setInput] = useState("");
+
+  const addToDo = (e) => {
+    e.preventDefault();
+    setToDos([...toDos, input]);
+    setInput("");
+  };
+
+  return (
+    <div className="app">
+      <h2>Welcome to my ToDo App</h2>
+      <form action="">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button type="submit" onClick={addToDo}>
+          Add ToDo
+        </button>
+      </form>
+
+      <h3>List of ToDos</h3>
+      {toDos.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
